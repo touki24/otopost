7793cb23942e1665eac6cbcfc757f1e0a93636b3
touki24 <iman.imantoko@gmail.com>
2021-10-18 00:33:29 +0700

    feat(): create post recycler adapter and implement it on post fragment

--- app/src/main/java/com/touki/otopost/presentation/post/PostFragment.kt
+++ app/src/main/java/com/touki/otopost/presentation/post/PostFragment.kt
@@ -2,21 +2,36 @@ package com.touki.otopost.presentation.post
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.touki.otopost.R
 import com.touki.otopost.core.post.model.Post
 import com.touki.otopost.databinding.FragmentPostBinding
+import com.touki.otopost.presentation.post.adapter.PostRecyclerAdapter
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class PostFragment : Fragment() {
 
+    companion object {
+        private const val TAG = "POST-FRAG"
+    }
     private val binding: FragmentPostBinding by viewBinding(createMethod = CreateMethod.INFLATE)
     private val viewModel: PostViewModel by sharedViewModel()
+    private val adapter by lazy {
+        PostRecyclerAdapter()
+    }
+
+    private val recyclerItemClickListener by lazy {
+        object : PostRecyclerAdapter.ItemClickListener {
+            override fun onClick(post: Post) {
+                Log.d(TAG, "onClick: $post")
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,10 +42,22 @@ class PostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupPostsRecycler()
+        setupPostsObserver()
         viewModel.fetchPosts()
-        viewModel.posts.observe(viewLifecycleOwner, {
-            it.forEach { post ->
-                Log.d("PostFrag", post.toString())
+    }
+
+    private fun setupPostsRecycler() {
+        adapter.setItemClickListener(recyclerItemClickListener)
+        binding.recyclerPost.adapter = adapter
+        binding.recyclerPost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+    }
+
+    private fun setupPostsObserver() {
+        viewModel.posts.observe(viewLifecycleOwner, { posts ->
+            adapter.setPosts(posts = posts)
+            posts.forEach { post ->
+                Log.d(TAG, post.toString())
             }
         })
     }
