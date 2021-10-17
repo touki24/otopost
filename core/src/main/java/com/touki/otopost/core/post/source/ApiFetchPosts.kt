5345b8f13d2411593582ad42a434ee33c0f8f95e
touki24 <iman.imantoko@gmail.com>
2021-10-17 21:52:11 +0700

    refactor(core): rename PostApiService to ApiFetchPosts

--- core/src/main/java/com/touki/otopost/core/post/source/ApiFetchPosts.kt
+++ core/src/main/java/com/touki/otopost/core/post/source/ApiFetchPosts.kt
@@ -3,6 +3,6 @@ package com.touki.otopost.core.post.source
 import com.touki.otopost.common.result.CommonResult
 import com.touki.otopost.core.post.model.Post
 
-interface PostApiService {
-    fun fetchPosts(): CommonResult<List<Post>>
+interface ApiFetchPosts {
+    suspend operator fun invoke(): CommonResult<List<Post>>
 }
