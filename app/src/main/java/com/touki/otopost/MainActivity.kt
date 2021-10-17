06f7772c6d6ffbc6674975b03d85239895985c52
touki24 <imant.imantoko@gmail.com>
2021-10-17 19:32:08 +0700

    first commit

---
+++ app/src/main/java/com/touki/otopost/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.touki.otopost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
