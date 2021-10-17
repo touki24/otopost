483e52bc349271e55c8dc932556014ec0203bae7
touki24 <iman.imantoko@gmail.com>
2021-10-17 22:29:27 +0700

    feat(): create api fetch post impl dummy and postmapper response to model

--- framework/src/main/java/com/touki/otopost/framework/core/post/source/ApiFetchPostsImpl.kt
+++ framework/src/main/java/com/touki/otopost/framework/core/post/source/ApiFetchPostsImpl.kt
@@ -3,9 +3,45 @@ package com.touki.otopost.framework.core.post.source
 import com.touki.otopost.common.result.CommonResult
 import com.touki.otopost.core.post.model.Post
 import com.touki.otopost.core.post.source.ApiFetchPosts
+import com.touki.otopost.framework.core.post.mapper.PostMapper
+import com.touki.otopost.framework.core.post.model.FetchPostsResponse
 
 class ApiFetchPostsImpl: ApiFetchPosts {
-    override suspend fun invoke(): CommonResult<List<Post>> {
-        return CommonResult.Success(listOf())
+    private val mapper by lazy {
+        PostMapper()
     }
+
+    override suspend fun invoke(): CommonResult<List<Post>> {
+        val posts = dummyFetchPostsResponse.map { item ->
+            mapper.responseToModel(item)
+        }
+        return CommonResult.Success(posts)
+    }
+}
+
+val dummyFetchPostsResponse = FetchPostsResponse().apply {
+    add(FetchPostsResponse.Item(
+        id = 1,
+        title = "Hello world",
+        content = "Hello world dang danga",
+        publishedAt = "2021-10-13T05:07:57.208Z",
+        createdAt = "2021-10-13T05:07:52.434Z",
+        updatedAt = "2021-10-15T02:25:07.912Z"
+    ))
+    add(FetchPostsResponse.Item(
+        id = 2,
+        title = "Hello world",
+        content = "Hello world dang danga",
+        publishedAt = "2021-10-13T05:07:57.208Z",
+        createdAt = "2021-10-13T05:07:52.434Z",
+        updatedAt = "2021-10-15T02:25:07.912Z"
+    ))
+    add(FetchPostsResponse.Item(
+        id = 3,
+        title = "Hello world",
+        content = "Hello world dang danga",
+        publishedAt = "2021-10-15T11:11:49.139Z",
+        createdAt = "2021-10-15T11:11:49.140Z",
+        updatedAt = "2021-10-15T11:11:49.139Z"
+    ))
 }
