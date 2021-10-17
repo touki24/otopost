1bdeddc912b9e93c3cf29eba8f50b66d2d00bf34
touki24 <iman.imantoko@gmail.com>
2021-10-17 23:09:37 +0700

    refactor(): create nav graph main and implement to main activity

--- app/src/main/java/com/touki/otopost/presentation/MainActivity.kt
+++ app/src/main/java/com/touki/otopost/presentation/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
