3156d668b1fad725090affbf68ada08b08fd0539
touki24 <iman.imantoko@gmail.com>
2021-10-18 01:28:25 +0700

    chore(): remove dummy response from fetch post impl

--- framework/src/main/java/com/touki/otopost/framework/core/post/source/ApiFetchPostsImpl.kt
+++ framework/src/main/java/com/touki/otopost/framework/core/post/source/ApiFetchPostsImpl.kt
@@ -37,30 +37,3 @@ internal class ApiFetchPostsImpl(private val httpClient: HttpClient): ApiFetchPo
         return CommonResult.Success(posts)
     }
 }
-
-internal val dummyFetchPostsResponse = FetchPostsResponse().apply {
-    add(FetchPostsResponse.Item(
-        id = 1,
-        title = "Hello world",
-        content = "Hello world dang danga",
-        publishedAt = "2021-10-13T05:07:57.208Z",
-        createdAt = "2021-10-13T05:07:52.434Z",
-        updatedAt = "2021-10-15T02:25:07.912Z"
-    ))
-    add(FetchPostsResponse.Item(
-        id = 2,
-        title = "Hello world",
-        content = "Hello world dang danga",
-        publishedAt = "2021-10-13T05:07:57.208Z",
-        createdAt = "2021-10-13T05:07:52.434Z",
-        updatedAt = "2021-10-15T02:25:07.912Z"
-    ))
-    add(FetchPostsResponse.Item(
-        id = 3,
-        title = "Hello world",
-        content = "Hello world dang danga",
-        publishedAt = "2021-10-15T11:11:49.139Z",
-        createdAt = "2021-10-15T11:11:49.140Z",
-        updatedAt = "2021-10-15T11:11:49.139Z"
-    ))
-}
