d9a94949999bff9a506a50cf179a0751bf791810
touki24 <iman.imantoko@gmail.com>
2021-10-18 10:59:19 +0700

    refactor(core): change apifetchpost return type from list post to post

--- core/src/main/java/com/touki/otopost/core/post/source/ApiFetchPost.kt
+++ core/src/main/java/com/touki/otopost/core/post/source/ApiFetchPost.kt
@@ -4,5 +4,5 @@ import com.touki.otopost.common.result.CommonResult
 import com.touki.otopost.core.post.model.Post
 
 interface ApiFetchPost {
-    suspend operator fun invoke(postId: Int): CommonResult<List<Post>>
+    suspend operator fun invoke(postId: Int): CommonResult<Post>
 }
