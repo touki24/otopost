f0ae390a3f56dd0d26bccf565522ebb723e527a3
touki24 <iman.imantoko@gmail.com>
2021-10-18 15:38:36 +0700

    feat(framework): add api delete post impementation

--- framework/src/main/java/com/touki/otopost/framework/core/post/repo/PostRepositoryImpl.kt
+++ framework/src/main/java/com/touki/otopost/framework/core/post/repo/PostRepositoryImpl.kt
@@ -3,12 +3,14 @@ package com.touki.otopost.framework.core.post.repo
 import com.touki.otopost.common.result.CommonResult
 import com.touki.otopost.core.post.model.Post
 import com.touki.otopost.core.post.repo.PostRepository
+import com.touki.otopost.core.post.source.ApiDeletePost
 import com.touki.otopost.core.post.source.ApiFetchPost
 import com.touki.otopost.core.post.source.ApiFetchPosts
 
 internal class PostRepositoryImpl(
     private val apiFetchPosts: ApiFetchPosts,
-    private val apiFetchPost: ApiFetchPost
+    private val apiFetchPost: ApiFetchPost,
+    private val apiDeletePost: ApiDeletePost
 ): PostRepository {
     override suspend fun fetchPosts(): CommonResult<List<Post>> {
         val posts = apiFetchPosts().fold(
@@ -24,6 +26,19 @@ internal class PostRepositoryImpl(
 
     override suspend fun fetchPost(postId: Int): CommonResult<Post> {
         val post = apiFetchPost(postId).fold(
+            success = { post ->
+                post
+            },
+            failure = {
+                return CommonResult.Failure(it)
+            }
+        )
+
+        return CommonResult.Success(post)
+    }
+
+    override suspend fun deletePost(postId: Int): CommonResult<Post> {
+        val post = apiDeletePost(postId).fold(
             success = { post ->
                 post
             },
---
+++ framework/src/main/java/com/touki/otopost/framework/core/post/source/ApiDeletePostImpl.kt
@@ -0,0 +1,34 @@
+package com.touki.otopost.framework.core.post.source
+
+import com.touki.otopost.common.result.CommonResult
+import com.touki.otopost.core.post.model.Post
+import com.touki.otopost.core.post.source.ApiDeletePost
+import com.touki.otopost.framework.BuildConfig
+import com.touki.otopost.framework.core.post.mapper.PostMapper
+import com.touki.otopost.framework.core.post.model.FetchPostResponse
+import com.touki.otopost.framework.http.Deserializer
+import com.touki.otopost.framework.http.HttpClient
+
+class ApiDeletePostImpl(private val httpClient: HttpClient): ApiDeletePost {
+    private val mapper by lazy {
+        PostMapper()
+    }
+
+    private val deserializer by lazy {
+        Deserializer<FetchPostResponse>(FetchPostResponse::class.java)
+    }
+
+    override suspend fun invoke(postId: Int): CommonResult<Post> {
+        val url = "${BuildConfig.BASE_URL}/posts/$postId"
+        val result = httpClient.delete(url).log("API-POST").dispatch(timeout = 10000, timeoutRead = 10000).fold(
+            success = {
+                deserializer.deserialize(it)
+            },
+            failure = { error ->
+                return CommonResult.Failure(error)
+            }
+        )
+
+        return CommonResult.Success(mapper.responseToModel(result))
+    }
+}
